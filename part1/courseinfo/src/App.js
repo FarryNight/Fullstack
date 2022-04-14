a7202689fc261c19e9da869f254e04e47d1553f2
FarryNight <36983130+FarryNight@users.noreply.github.com>
2022-04-14 15:14:20 +0800

    Complete exercises1.3-1.5 && upgrade react to 1.18.x

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,46 +1,56 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content 
-      part1 = {part1} exercises1 = {exercises1} 
-      part2 = {part2} exercises2 = {exercises2}
-      part3 = {part3} exercises3 = {exercises3}
-      />
-      <Total all ={exercises1+exercises2+exercises3}/>
+      <Header course = {course}/>
+      <Content content = {course.parts}/>
+      <Total num ={course.parts}/>
     </div>
   )
 }
-const Header = (props) => {
+const Header = (coursename) => {
+  //console.log(coursename.course.parts[0])
   return(
     <div>
-      <h1>{props.course}</h1>
+      <h1>{coursename.course.name}</h1>
     </div>
   )
 }
 
-const Content = (props) => <Part part={props} exercises={props}/>
-
-const Part = ({part,exercises}) => {
+const Content = (props) => {
+  //console.log(props.content.parts)
   return(
     <div>
-      <p>{part.part1} {exercises.exercises1}</p>
-      <p>{part.part2} {exercises.exercises2}</p>
-      <p>{part.part3} {exercises.exercises3}</p>
+      <Part parts={props.content[0]}/>
+      <Part parts={props.content[1]}/>
+      <Part parts={props.content[2]}/>
     </div>
   )
 }
+const Part = (part) => <p>{part.parts.name} {part.parts.exercises}</p>
 
-const Total = (props) => <p>{props.all}</p>
+const Total = (props) => <p>{props.num[0].exercises+
+  props.num[1].exercises+
+  props.num[2].exercises}
+  </p>
 
 export default App
--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,7 +1,12 @@
-import ReactDOM from 'react-dom'
+//import ReactDOM from 'react-dom'
+import React from "react";
+import {createRoot} from "react-dom/client"
 import App from './App'
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-ReactDOM.render(
-  <App />, 
-  document.getElementById('root')
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
