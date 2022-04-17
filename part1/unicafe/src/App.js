2ddbce53deafada5504450cd8c30266eb4254c37
FarryNight <36983130+FarryNight@users.noreply.github.com>
2022-04-18 01:29:37 +0800

    Del useless code

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,7 +4,6 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [selected, setSelected] = useState(0)
   return (
     <div>
       <div> <h3>Give Feedback</h3> </div>
